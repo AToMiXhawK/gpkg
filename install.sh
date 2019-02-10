9b9c22ae98df6ff054c6fff4b5a467dea70dc5a2
AToMiXhawK <githinmanesh@gmail.com>
2019-02-10 18:01:16 +0530

    Installer/Updater

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-echo -e "if [ -d "$HOME/gpkg" ] ; then
+if ! which git > /dev/null; then
+	echo "git not installed. Install it with your local package manager and try again!"
+	exit
+fi
+
+DIR=~/gpkg
+
+if [[ -d $DIR ]]
+then
+  	cd ~/gpkg
+  	git pull
+  	exit
+else
+	cd ~
+	git clone https://github.com/AToMiXhawk/gpkg ~/gpkg
+	echo -e "if [ -d "$HOME/gpkg" ] ; then
     PATH="$HOME/gpkg:$PATH"
-fi" >> ~/.profile
-source ~/.profile
+	fi" >> ~/.profile
+
+	source ~/.profile
+fi
+
+
+
